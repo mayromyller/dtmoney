91e7707e284aec08421b0dcd9ab5363fb41ed236
Mayro Myller <deehmyller11@gmail.com>
2021-10-15 10:28:51 -0300

    chore: configure miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import { GlobalStyles } from './styles/global'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
+import { App } from "./App";
+import { GlobalStyles } from "./styles/global";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "First Transaction",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <GlobalStyles />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
