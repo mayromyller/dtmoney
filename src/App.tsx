046180fb40350fea34330c2a3d2721fe8f7576d6
Mayro <deehmyller11@gmail.com>
2021-11-02 14:59:19 -0300

    chore: set transaction provider in app component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard/index";
 import { TransactionModal } from "./components/TransactionModal";
 
+import { TransactionProvider } from "./TransactionsProvider";
+
 Modal.setAppElement("#root");
 
 export function App() {
@@ -19,10 +21,10 @@ export function App() {
     setModal(false);
   }
   return (
-    <>
+    <TransactionProvider>
       <Header openModal={handleOpenModal} />
       <Dashboard />
       <TransactionModal isOpen={modal} onRequestClose={handleCloseModal} />
-    </>
+    </TransactionProvider>
   );
 }
